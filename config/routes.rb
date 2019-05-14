46621867d6bc5baf7533600b544850480addb247
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2019-05-14 17:59:54 +0700

    complete

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get 'stylish_portfolios/index'
+  root 'stylish_portfolios#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
